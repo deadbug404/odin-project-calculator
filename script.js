e4ea8378ed74de721b8c59d4a79a11e172daf542
nannoisdead <narisafeint@gmail.com>
2023-02-17 21:01:26 +0800

    add clear and delete function

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 //getting all the dom elements
 let buttons = document.querySelectorAll('button');
 let screen = document.querySelector('.display-value');
+let totalScreen = document.querySelector('.total-value');
+
+
 buttons.forEach(button => {
-    button.addEventListener('click',display);
+    let id = button.id;
+    if(id == 'del'){
+        button.addEventListener('click',del);
+    }else if(id == 'clear'){
+        button.addEventListener('click',clr);
+    }else{
+        button.addEventListener('click',display);
+    }
 });
+
 
 function add(x,y){
     return x+y;
@@ -37,15 +48,40 @@ function operate(operator,num1,num2){
 
 function display(e){
     let value = e.target.id;
+    let prevTotal = parseInt(totalScreen.textContent);
     let total = parseInt(screen.textContent);
     if(screen.textContent.length < 23){
-        if(total == 0){
+        if(value == '+' || value == '-' || value == '/' || value == '*'){
+            totalScreen.textContent = total;
             screen.textContent = value;
         }else{
-            screen.textContent += value;
+            if(total == 0) {
+                screen.textContent = value;
+            }else{
+                if(Number.isInteger(parseInt(screen.textContent))){
+                    screen.textContent += value;
+                }else{
+                    screen.textContent = value;
+                }
+            }
         }
     }else{
         alert('Maximum limit reached');
     }
     
+}
+
+function del(){
+    let newString = screen.textContent.substring(0,screen.textContent.length-1);
+    if(screen.textContent.length == 1){
+        screen.textContent = newString;
+        screen.textContent = 0;
+    }else{
+        screen.textContent = newString;
+    }
+}
+
+function clr(){
+    screen.textContent = '0';
+    totalScreen.textContent = '';
 }
